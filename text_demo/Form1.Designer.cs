9383ca15679911fcbc21dd3c5b8d01d0832c2cb1
emil9280 <33316283+emil9280@users.noreply.github.com>
2017-12-19 00:06:54 +0100

    demo1

--- text_demo/Form1.Designer.cs
+++ text_demo/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Add_vine = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CB_V_N = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.TB_new = new System.Windows.Forms.TextBox();
+            this.BU_RE = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // save
+            // 
+            this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.save.Location = new System.Drawing.Point(201, 279);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 0;
+            this.save.Text = "S&alg";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Add_vine
+            // 
+            this.Add_vine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Add_vine.Location = new System.Drawing.Point(12, 252);
+            this.Add_vine.Name = "Add_vine";
+            this.Add_vine.Size = new System.Drawing.Size(75, 23);
+            this.Add_vine.TabIndex = 5;
+            this.Add_vine.Text = "A&dd vine";
+            this.Add_vine.UseVisualStyleBackColor = true;
+            this.Add_vine.Click += new System.EventHandler(this.Add_vine_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 131);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CB_V_N
+            // 
+            this.CB_V_N.FormattingEnabled = true;
+            this.CB_V_N.Location = new System.Drawing.Point(22, 166);
+            this.CB_V_N.Name = "CB_V_N";
+            this.CB_V_N.Size = new System.Drawing.Size(121, 21);
+            this.CB_V_N.TabIndex = 9;
+            this.CB_V_N.SelectedIndexChanged += new System.EventHandler(this.CB_V_N_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(204, 167);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // TB_new
+            // 
+            this.TB_new.Location = new System.Drawing.Point(12, 281);
+            this.TB_new.Name = "TB_new";
+            this.TB_new.Size = new System.Drawing.Size(75, 20);
+            this.TB_new.TabIndex = 11;
+            // 
+            // BU_RE
+            // 
+            this.BU_RE.Location = new System.Drawing.Point(201, 250);
+            this.BU_RE.Name = "BU_RE";
+            this.BU_RE.Size = new System.Drawing.Size(75, 23);
+            this.BU_RE.TabIndex = 12;
+            this.BU_RE.Text = "R&estock";
+            this.BU_RE.UseVisualStyleBackColor = true;
+            this.BU_RE.Click += new System.EventHandler(this.BU_RE_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(301, 314);
+            this.Controls.Add(this.BU_RE);
+            this.Controls.Add(this.TB_new);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.CB_V_N);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Add_vine);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.save);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Dansk Vin Import lager";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Add_vine;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox CB_V_N;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox TB_new;
+        private System.Windows.Forms.Button BU_RE;
     }
 }
 
--- text_demo/Form1.cs
+++ text_demo/Form1.cs
@@ -7,14 +7,110 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace text_demo
 {
     public partial class Form1 : Form
     {
+        private List<string> Name1 = new List<string>();
+        private List<int> Amount = new List<int>();
         public Form1()
         {
             InitializeComponent();
+            Read_From();
+            foreach(string element in Name1)
+            {
+                CB_V_N.Items.Add(element);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            Amount[CB_V_N.SelectedIndex] -= (int)numericUpDown1.Value;
+            label1.Text = Amount[CB_V_N.SelectedIndex].ToString();
+            Write_To();
+            numericUpDown1.Value = 0;
+        }
+        private void Write_To()
+        {
+            TextWriter txt = new StreamWriter("C:\\Users\\Emilm\\Desktop\\New folder\\demo.txt");
+            for (int i = 0; i < Name1.Count; i++)
+            {
+                txt.WriteLine("");
+                txt.WriteLine(Name1[i]);
+                txt.WriteLine(Amount[i]);                
+            }
+            txt.Close();
+        }
+        private void Read_From()
+        {
+            TextReader tet = new StreamReader("C:\\Users\\Emilm\\Desktop\\New folder\\demo.txt");
+            while (tet.ReadLine() != null)
+            {
+                Name1.Add(tet.ReadLine());
+                Amount.Add(Convert.ToInt32(tet.ReadLine()));                
+            }
+            tet.Close();
+        }
+
+        private void Add_vine_Click(object sender, EventArgs e)
+        {
+            if (TB_new.Text != "")
+            {
+                Name1.Add(TB_new.Text);
+                Amount.Add(0);
+                CB_V_N.Items.Add(TB_new.Text);
+                TB_new.Text = "";
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BU_RE_Click(object sender, EventArgs e)
+        {
+            Amount[CB_V_N.SelectedIndex] += (int)numericUpDown1.Value;
+            label1.Text = Amount[CB_V_N.SelectedIndex].ToString();
+            Write_To();
+            numericUpDown1.Value = 0;
+        }
+
+        private void CB_V_N_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = Amount[CB_V_N.SelectedIndex].ToString();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
